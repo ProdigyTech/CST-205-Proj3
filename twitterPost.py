0f62b1ee74fa50ad7d6f2e34d6c56391968b38fd
Javar Alexander <jaalexander@csumb.edu>
2017-05-11 18:23:55 +0000

    tweet set up

--- twitterPost.py
+++ twitterPost.py
@@ -1,16 +1,15 @@
 import tweepy
 
 ###### TWITTER API #############################################
-consumer_key= "7EadEmQhNj4j6RY82f2HxhI2n" 
-consumer_secret= "1eZhg5WNEO8Jt09E9dWy9Mo6f9Mriz83utvx8tb5lXJMwserkC" 
-access_token = "1518510956-gU73yOwXZf20D2whzQ7CVIuGxObIQkEhSa6yD5j" 
-access_token_secret = "WyvCoPFt0HwYLjqiUkvBHz4RA67vmyOKBbwI0vhD11LsJ" 
 
-#Logging into twitter using the tweetpy wrapper
-auth = tweepy.OAuthHandler(consumer_key,  consumer_secret)
-auth.set_access_token(access_token, access_token_secret)
-api = tweepy.API(auth)
-
-tweet = "Another test using a string"
-
-# api.update_status(tweet)
+def sendTweet(tweetData):
+    
+    consumer_key= "EuvpBtbfkdpycmVxR14gH7mph" 
+    consumer_secret= "jr8t52Xof1bliEQIFD5MUe1qK2eYEnBOrG7T5QReanW125KgAs" 
+    access_token = "862722342501232640-E1p1EwoAOxZlgbRGWCUELKmgkrQpvLx" 
+    access_token_secret = "LrvxY0HLqsxsYNvpUUi8kyEk0xMkjPgQz6lrO0TEz5AMC" 
+    #Logging into twitter using the tweetpy wrapper
+    auth = tweepy.OAuthHandler(consumer_key,  consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
+    api = tweepy.API(auth)
+    api.update_status(tweetData)
